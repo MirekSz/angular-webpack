ba0b665ecb7ad6479f139ba0ab4ed00bf00736ac
MirekSz <miro1994>
2018-08-12 12:41:25 +0200

    init

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,5 @@
-import angular from 'angular';
-
 import '../style/app.css';
-
+import template from './dialog1.tmpl.html'
 let app = () => {
   return {
     template: require('./app.html'),
@@ -11,14 +9,40 @@ let app = () => {
 };
 
 class AppCtrl {
-  constructor() {
+  constructor($scope, $mdDialog, $timeout) {
     this.url = 'https://github.com/preboot/angular-webpack';
+    this.$mdDialog = $mdDialog;
+    $timeout(() => {
+      this.show = true;
+    }, 1000);
   }
+  showAlert() {
+    // Appending dialog to document.body to cover sidenav in docs app
+    // Modal dialogs should fully cover application
+    // to prevent interaction outside of dialog
+    this.$mdDialog.show(
+      this.$mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title('This is an alert title')
+        .textContent('You can specify some description text in here.')
+        .ariaLabel('Alert Dialog Demo')
+        .ok('Got it!')
+    );
+  };
+  showAdvanced() {
+    this.$mdDialog.show({
+      template: template,
+      parent: angular.element(document.body),
+      clickOutsideToClose: true,
+      fullscreen: false
+    });
+  };
 }
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, ['ngMaterial', 'ngMessages'])
   .directive('app', app)
   .controller('AppCtrl', AppCtrl);
 
